7ed52f17b58b48dfbd3e0d254ca26039ecbcfe20
khamb <khadim1996@icloud.com>
2018-02-04 20:22:12 -0500

    minor change

--- jeCalc/ViewController.swift
+++ jeCalc/ViewController.swift
@@ -9,6 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    func calculate(l: Int, r: Int, sign: String) -> Int {
+        var result: Int = 0
+        switch sign {
+        case "+":
+            result = l+r
+        case "-":
+            result = l-r
+        case "x":
+            result = l*r
+        case "÷":
+            result = l/r
+        default:
+            break
+        }
+        return result
+    }
+    
+    @IBAction func getResult(_ sender: UIButton) {
+        let chain: String = resultLabel.text!
+        var left: String = ""
+        var right: String = ""
+        var sign: String = ""
+        
+        for c in chain {
+            if c != "+" && c != "-" && c != "÷" && c != "x" && sign == "" {
+                left += String(c)
+            } else if c != "+" && c != "-" && c != "÷" && c != "x" && sign != "" {
+                right = String(c)
+            }else {
+                sign = String(c)
+            }
+        }
+        
+        
+        resultLabel.text! = String(calculate(l: Int(left)!, r: Int(right)!, sign: sign))
+        
+
+        
+    }
+    
+    @IBAction func clearButton(_ sender: Any) {
+        resultLabel.text! = "0"
+    }
+    
+    @IBAction func onNumberPressed(_ sender: UIButton) {
+        
+        if resultLabel.text! == "0" {
+            resultLabel.text! = sender.currentTitle!
+        }else{
+            resultLabel.text! = resultLabel.text! + sender.currentTitle!
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
